b4c9d0480277826a0746edd6960b1db357909f60
eloizameurermartins-stack <eloiza.meurer.martins@escola.pr.gov.br>
2026-08-10 09:56:34 -0300

    2

---
+++ script.js
@@ -0,0 +1,76 @@
+constㅤbotoesㅤ=document.querySelectorAllㅤ("button");
+botoes.forEach(function(botao)){
+
+
+letㅤcurtiu=false;
+    botao.addEventListenerㅤ("click",ㅤbotaoClicado);
+    functionㅤbotaoClicado(){
+ console.log("fuiㅤclicado");
+        letㅤtexto=botao.querySeletor("span");
+        if(curtiuㅤ===ㅤfalse){
+            texto.textcontentㅤ++;
+          curtiuㅤ=ㅤtrue;
+            }ㅤelseㅤ{
+                texto.textcontent;
+                curtiu=false;
+            }
+       
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+    
+
+
+    
